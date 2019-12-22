73f7a084afb8404980857b95acb25916810bc896
Agnes Kis <agi.uqbar@gmail.com>
2019-12-22 13:15:50 +0100

    Set up TravisCI.

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestGhostGame(unittest.TestCase):
+
+    def test_first_testcase(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
